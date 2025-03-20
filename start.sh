d8a05cacdcd6f5e7d36779b4f9dd3b10eb78ca7c
Timofei <email@yandex.ru>
2025-03-20 09:08:10 +0300

    Добавлен start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Завершение работы при обнаружении ошибок
+set -eu
+
+# Python буферизует стандартный вывод. Без этого вы не увидите, что вы "печатаете" в журналах действий
+export PYTHONUNBUFFERED=true
+
+# Установка Python 3 в виртуальную среду
+VIRTUALENV=./venv
+
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
+fi
+
+# Установка pip в виртуальную среду
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+fi
+
+# Установка requirements
+$VIRTUALENV/bin/pip install -r requirements.txt
+
+# Запуск нашего приложения
+$VIRTUALENV/bin/python3 main.py
